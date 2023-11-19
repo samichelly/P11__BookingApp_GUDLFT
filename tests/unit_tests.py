df3cb53ba102f4a0b9765211f3485ac4354a3e64
Sami Chelly <samichelly15@gmail.com>
2023-11-19 19:18:16 +0100

    correction purchasePlaces, failed test_deduct_points

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -13,30 +13,29 @@ def client():
 
 def test_deduct_points(client):
     # Données de test
+    # competition = [
+    #     {"name": "Test", "date": "2023-11-27 10:00:00", "numberOfPlaces": "25"}
+    # ]
+    # club = [{"name": "Test club", "email": "test@mail.com", "points": "10"}]
     competition = [
-        {"name": "Test", "date": "2023-11-27 10:00:00", "numberOfPlaces": "25"}
+        {"name": "Fall Classic", "date": "2020-10-22 13:30:00", "numberOfPlaces": "13"}
     ]
-    club = [{"name": "Test club", "email": "testclub@email.com", "points": "10"}]
+    club = [{"name": "She Lifts", "email": "kate@shelifts.co.uk", "points": "12"}]
+
+    club[0]["points"] = int(club[0]["points"])
+
+    print("club : ", club)
 
     # Configuration initiale
     with app.app_context():
         app.competitions = competition
         app.clubs = club
 
-    # Réservation de places
     club_points_before = int(club[0]["points"])
     places_booked = 3
 
-    response = client.post(
-        "/purchasePlaces",
-        data={
-            "places": places_booked,
-            "club": club[0]["name"],
-            "competition": competition[0]["name"],
-        },
-    )
+    print(type(club_points_before), club_points_before)
+    print(type(places_booked), places_booked)
+    print(type(club[0]["points"]), club[0]["points"])
 
-    # Assertions
-    assert response.status_code == 200
-    assert "Great-booking complete!" in response.data.decode()
-    assert int(club[0]["points"]) == club_points_before - places_booked
+    assert club[0]["points"] == club_points_before - places_booked
